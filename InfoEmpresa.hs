81dea44560344504e163a56facd3b946f163b6d3
JohanV146 <92075881+JohanV146@users.noreply.github.com>
2023-10-05 00:12:11 -0600

    Add files via upload
    ahora se manejan correctamente los datos y ademas se cargan los datos desde un txt

--- InfoEmpresa.hs
+++ InfoEmpresa.hs
@@ -1,47 +1,72 @@
-data Empresa = Empresa
-    { 
-        nombreEmpresa :: String,
-        sitioWeb :: String,
-        contacto :: String,
-        tarifaPedal :: Int,
-        tarifaElectrico :: Int,
-        tarifaGasolina :: Int
-    }
+module InfoEmpresa where
 
--- Función para validar si una cadena es un número de 8 dígitos
-esNumeroValido :: String -> Bool
-esNumeroValido s = length s == 8 && all (\c -> c >= '0' && c <= '9') s
+-- ======== Registro de empreza ==================================================================================
+type NombreEmpresa = String
+type SitioWeb = String
+type Contacto = String
+type TarifaPedal = Int
+type TarifaElectrico = Int
+type TarifaGasolina = Int
 
--- Función para registrar la información de la empresa
-registrarEmpresa :: IO Empresa
-registrarEmpresa = do
-    putStrLn "Ingrese el nombre de la empresa:"
-    nombre <- getLine
-    putStrLn "Ingrese el sitio web de la empresa:"
-    web <- getLine
-    putStrLn "Ingrese el número de contacto (8 dígitos):"
-    contactoInfo <- getLine
-    if esNumeroValido contactoInfo
-        then do
-            putStrLn "Ingrese la tarifa por kilómetro pedal (TR):"
-            tarifaTR <- readLn
-            putStrLn "Ingrese la tarifa por kilómetro eléctrico (AE):"
-            tarifaAE <- readLn
-            putStrLn "Ingrese la tarifa por kilómetro gasolina (AG):"
-            tarifaAG <- readLn
+data Empresa = Empresa NombreEmpresa SitioWeb Contacto TarifaPedal TarifaElectrico TarifaGasolina;
+-- ===============================================================================================================
+-- ======== Constructor de data Empreza y gets ===================================================================
+crearEmpresa(elemento) = Empresa (elemento!!0) (elemento!!1) (elemento!!2) (read (elemento!!3) :: Int) (read (elemento!!4) :: Int) (read (elemento!!5) :: Int)
+getNombreEmpresa (Empresa nombre _ _ _ _ _) = nombre;
+getSitioWeb (Empresa _ sitioWeb _ _ _ _) = sitioWeb;
+getContacto (Empresa _ _ contacto _ _ _) = contacto;
+getTarifaPedal (Empresa _ _ _ tarifaPedal _ _) = tarifaPedal;
+getTarifaElectrico (Empresa _ _ _ _ tarifaElectrico _) = tarifaElectrico;
+getTarifaGasolina (Empresa _ _ _ _ _ tarifaGasolina) = tarifaGasolina;
 
-            let empresa = Empresa
-                    { 
-                        nombreEmpresa = nombre,
-                        sitioWeb = web,
-                        contacto = contactoInfo,
-                        tarifaPedal = tarifaTR,
-                        tarifaElectrico = tarifaAE,
-                        tarifaGasolina = tarifaAG
-                    }
+-- ======== Convierte a String los datos de una data Empresa =====================================================
+showEmpresa :: Empresa -> [Char]
+showEmpresa empresa =
+            let
+                nombre = getNombreEmpresa(empresa)
+                web = getSitioWeb(empresa)
+                contac = getContacto(empresa)
+                pedal = getTarifaPedal(empresa)
+                electrico = getTarifaElectrico(empresa)
+                gasolina = getTarifaGasolina(empresa)
+            in 
+                "Nombre de la Empresa: " ++ nombre ++ "\n" ++
+                "Sitio Web: " ++ web ++ "\n" ++
+                "Contacto: " ++ contac ++ "\n" ++
+                "Tarifa para Pedal: " ++ show pedal ++ "\n" ++
+                "Tarifa para Electrico: " ++ show electrico ++ "\n" ++
+                "Tarifa para Gasolina: " ++ show gasolina
 
-            putStrLn "Información de la empresa registrada con éxito."
-            return empresa
-        else do
-            putStrLn "Número de contacto no válido. Debe tener 8 dígitos."
-            registrarEmpresa
+-- ======== Muestra todas las data Empresa de una lista ===========================================
+showEmpresas :: [Empresa] -> IO ()
+showEmpresas lista =
+                putStrLn(showEmpresa (head lista))
+
+-- ======== Split por comas ========================================================================
+separarPorComas :: ([Char], [Char]) -> [[Char]]
+separarPorComas (cadena, temp) =
+    if cadena == "" then [temp] else
+        if (head cadena) == (head ",") then
+            [temp] ++ separarPorComas ((tail cadena), "")
+            else
+                separarPorComas((tail cadena), temp++[(head cadena)])
+
+-- ======== Separa por comas una linea y lo convierte en data tipo persona =========================
+separarElementos :: [[Char]] -> [Empresa]
+separarElementos lista =
+    if lista == [] then
+        []
+    else
+        [crearEmpresa(separarPorComas((head lista), ""))] ++ separarElementos (tail lista)
+
+-- ======== Convierte en una lista un texto, parte el texto por cambios de lineas ===================
+convierteAlineas :: String -> [String]
+convierteAlineas texto = lines texto
+
+-- ======== Lee todo el contenido de archivo, los convierte en lineas ================================
+-- ======== luego parsea por comas las lineas y genera elementos persona =============================
+leerArchivo :: FilePath -> IO [Empresa]
+leerArchivo archivo = do
+    contenido <- readFile archivo
+    let empresas = separarElementos (convierteAlineas contenido)
+    return empresas
